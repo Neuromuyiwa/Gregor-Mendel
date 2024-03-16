97fbf86f8b2151bae4fe34303d395e1c47af8dc1
otiendephan <163111525+otiendephan@users.noreply.github.com>
2024-03-16 15:20:25 +0300

    Add files via upload

---
+++ script/phanice_working_codes.sh
@@ -0,0 +1,229 @@
+echo hello
+mkdir directory_phanice
+mkdir phanice
+Help
+"gcloud config set project [PROJECT_PHANICE]"
+echo " hello there"
+"date"
+"calender"
+" Calender "
+clear
+pwd
+ls
+cd
+cd.
+cd..
+cd
+mkdr phanice
+mkdir phanice
+mkdir biocomputing
+mkdir test1
+mkdir test2
+touch otiende
+ls
+mkdir Phanice
+echo mkdir phanice
+clear
+mkdir Phanice
+mkdir biocomputing
+ls
+cd ~
+ls ~
+pwd
+ls
+rm -r test2
+LS
+ls
+rm -r test1
+rm -r Phanice
+ls
+rm -r README-cloudshell.txt
+ls
+rm -r phanice
+rm -r otiende
+rm -r directory_phanice
+ls
+rm -r biocomputing
+ls
+mkdir phanice
+ls
+clear
+$ mkdir -p phanice/biocomputing && cd phanice/biocomputing
+mkdir -p phanice/biocomputing && cd phanice/biocomputing
+ls
+cd..
+cd
+ls
+ls~
+cd ~
+cd.
+clear
+cd.
+cd..
+ls
+pwd
+ls
+mkdir biocomputing && cd biocomputing
+wget -O wildtype.fna https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+wget -O wildtype.gbk https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+mv wildtype.fna phanice
+dir
+cd..
+cd
+cd phanice
+dir
+ls
+cd
+dir
+cd phanice
+ls
+cd
+cd biocomputing
+ls
+cd
+mv wildtype.fna ~/phanice
+mv wildtype.fna phanice
+find . -type f -name "wildtype.fna"
+ls
+cd phanice
+ls
+cd biocomputing
+ls
+clear
+cd..
+cd
+ls
+cd biocomputing
+ls
+wget -O wildtype.fna https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+ls
+mv wildtype.fna phanice
+ls
+cd phanice
+cd
+cd phanice
+ls
+cd
+biocomputing
+cd biocomputing
+ls
+cd
+ls
+cd phanice
+ls
+rm biocomputing
+cd
+rm biocomputing
+rm dir biocomputing
+rm -r biocomputing
+ls
+cd phanice
+cd
+mkdir biocomputing
+ls
+cd biocomputing
+ls
+wget -O wildtype.fna https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+ls
+wget -O wildtype.gbk https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+ls
+mv wildtype.fna /phanice
+ls
+mv wildtype.fna ~/phanice
+cd
+cd phanice
+ls
+cd
+cd biocomputing
+ls
+rm wildtype.gbk
+ls
+help
+cloudshell help
+dl https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+clear
+$ wget -O wildtype.fna https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+clear
+clera
+clear
+ls
+#
+clear
+#!/bin/bash
+wget -O wildtype.fna https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+*wildtype.fna
+dl https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+cat wildtype.fna.txt
+unzip wildtype.fna.zip
+unzip wildtype.zip
+wildtype.fna zip
+clear
+ls
+cd biocomputing
+ls
+cd
+cd phanice
+ls
+cat wildtype.fna
+if grep -q 'tatatata' wildtype.fna; then     echo "Mutant: Contains tatatata"; else     if grep -q 'tata' wildtype.fna; then         echo "Wildtype: Contains tata"     fi nano wildtype.fna; cd; exitcd..; stop; cd:; ls~; clear; q; help; clear
+cat wildtype.fna
+nano mutant_or_wildtype.sh
+chmod +x check_file_type.sh
+chmod +x mutant_or_wildtype.sh
+./mutant_or_wildtype.sh
+grep --color=always -E 'tata|tatatata' wildtype.fna | sed -E "s/(tata)/\o033[31m\1\o033[0m/g;s/(tatatata)/\o033[32m\1\o033[0m/g"
+./mutant_or_wildtype.sh
+grep 'tatatata' wildtype.fna > mutant_present.txt
+cat mutant_present.txt
+grep 'tatatata' wildtype.fna | sed 's/tatatata/\\e[31m&\\e[0m/g' > mutant_lines.txt
+cat mutant_present.txt
+grep 'tatatata' wildtype.fna | sed 's/tatatata/\\e[31m&\\e[0m/g' > mutant_present.txt
+cat mutant_present.txt
+grep 'tatatata' wildtype.fna | sed -E 's/tatatata/\x1b[31m&\x1b[0m/g' > mutant_present.txt
+cat mutant_present.txt
+clear
+$ wget -O hemoglobin.fasta "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000011.10
+&rettype=fasta&retmode=text"
+wget -O hemoglobin.fasta "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000011.10
+&rettype=fasta&retmode=text"
+cat hemoglobin.fasta
+grep -o 'A' hemoglobin.fasta | wc -l
+grep -o 'G' hemoglobin.fasta | wc -l
+grep -o 'C' hemoglobin.fasta | wc -l
+grep -o 'T' hemoglobin.fasta | wc -l
+tail -n +2 hemoglobin.fasta | wc -l
+clear
+nano GC_content.sh
+chmod +x GC_content.sh
+./GC_content.sh
+nano GC_content.sh
+fg
+rm GC_content.sh
+nano calculate_GC.sh
+chmod +x calculate_GC.sh
+./calculate_GC.sh
+nano calculate_GC.sh
+./calculate_GC.sh
+touch phanice
+echo>> phanice
+ls
+cd phanice
+ls
+rm phanice
+ls
+touch phanice.fna
+ls
+cat phanice.fna
+cd phanice.fna
+cd phanice
+ls
+cd phanice.fna
+nano  phanice.fna
+echo "A=39286731" >> phanice.fna
+cat phanice.fna
+echo "G=27981802" >> phanice.fna
+cat phanice.fna
+echo "T=39361954" >> phanice.fna
+cat phanice.fna
+echo "C=27903259" >> phanice.fna
+cat phanice.fna
